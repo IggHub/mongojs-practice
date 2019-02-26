fafb6350227e840840f65468d4a2554a4b680e81
Iggy <igoririanto@rocketmail.com>
2019-02-26 08:02:22 -0600

    add body-parser

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 
 app.listen(process.env.PORT);
